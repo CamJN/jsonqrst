006852a475d3c7ebc374f61052e2f2eb5e89264a
Camden Narzt <c.narzt@me.com>
2017-07-18 10:52:23 -0600

    use IO

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,13 @@
-extern crate serde_json;
+#[macro_use] extern crate serde_json;
 
 use serde_json::{Value, Error};
+use std::io::Read;
 
-fn untyped_example(data: &str, query: &str) -> Result<Value, Error> {
+fn apply_query(data: &str, query: &str) -> Result<Value, Error> {
     let mut v = serde_json::from_str(data);
 
     let r:&mut Value = v.as_mut().unwrap();
 
-    // corner cases: . in string
     let ret = query.split(".").fold(r,|acc,i| match i.parse::<usize>() {
         Ok(i) => acc.get_mut(i).expect(&format!("index {} doesn't exist",i)),
         Err(_) => acc.get_mut(i).expect(&format!("index {} doesn't exist",i))
@@ -16,15 +16,36 @@ fn untyped_example(data: &str, query: &str) -> Result<Value, Error> {
 }
 
 fn main() {
-    // Some JSON input data as a &str. Maybe this comes from the user.
-    let data = r#"{
-                    "name": "John Doe",
-                    "age": 43,
-                    "phones": [
-                      "+44 1234567",
-                      "+44 2345678"
-                    ]
-                  }"#;
-    let query = "phones.0";
-    println!("{}",untyped_example(data, query).expect("Error parsing"));
+    let query = std::env::args().skip(1).fold(String::new(),|mut s,a|{s.push_str(&a);s});
+
+    let mut data = String::new();
+    let stdin = std::io::stdin();
+    let _:usize = stdin.lock().read_to_string(&mut data).expect("failed to read stdin");
+
+    println!("{}",apply_query(&data, &query).expect("Error parsing"));
+}
+
+
+// idea: allow * as wildcard for a level (might only make sense for arrays, i dunno)
+// idea: allow multiple queries
+// corner case: . in string (allow escaping?)
+
+#[cfg(test)]
+mod tests {
+    use super::apply_query;
+
+    #[test]
+    fn get_value() {
+        let data = r#"{
+                        "name": "John Doe",
+                        "age": 43,
+                        "phones": [
+                          "+44 1234567",
+                          "+44 2345678"
+                        ]
+                      }"#;
+
+        assert_eq!(json!("+44 1234567"), apply_query(data, "phones.0").unwrap());
+        assert_eq!(json!("John Doe"), apply_query(data, "name").unwrap());
+    }
 }
